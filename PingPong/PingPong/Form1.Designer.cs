95186c22bc48843808648100771bc47fa34066c7
Janick Spielmann <janick.spielmann@messageme.ch>
2022-11-20 22:14:12 +0100

    ball can no longer run away

--- PingPong/PingPong/Form1.Designer.cs
+++ PingPong/PingPong/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PingPong
 {
-    partial class Form1
+    partial class frmPingPong
     {
         /// <summary>
         /// Required designer variable.
@@ -29,12 +29,73 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.pnlSpiel = new System.Windows.Forms.Panel();
+            this.picBall = new System.Windows.Forms.PictureBox();
+            this.tmrSpiel = new System.Windows.Forms.Timer(this.components);
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.pnlSpiel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 415);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Spiel starten";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // pnlSpiel
+            // 
+            this.pnlSpiel.BackColor = System.Drawing.Color.SeaGreen;
+            this.pnlSpiel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlSpiel.Controls.Add(this.picBall);
+            this.pnlSpiel.Location = new System.Drawing.Point(40, 40);
+            this.pnlSpiel.Name = "pnlSpiel";
+            this.pnlSpiel.Size = new System.Drawing.Size(600, 300);
+            this.pnlSpiel.TabIndex = 2;
+            // 
+            // picBall
+            // 
+            this.picBall.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.picBall.Location = new System.Drawing.Point(489, 183);
+            this.picBall.Name = "picBall";
+            this.picBall.Size = new System.Drawing.Size(25, 25);
+            this.picBall.TabIndex = 3;
+            this.picBall.TabStop = false;
+            // 
+            // tmrSpiel
+            // 
+            this.tmrSpiel.Interval = 60;
+            this.tmrSpiel.Tick += new System.EventHandler(this.tmrSpiel_Tick);
+            // 
+            // frmPingPong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.pnlSpiel);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmPingPong";
+            this.Text = "Ping-Pong Spiel";
+            this.pnlSpiel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Panel pnlSpiel;
+        private System.Windows.Forms.Timer tmrSpiel;
+        private System.Windows.Forms.PictureBox picBall;
+        private System.Windows.Forms.BindingSource bindingSource1;
     }
 }
 
--- PingPong/PingPong/Form1.cs
+++ PingPong/PingPong/Form1.cs
@@ -10,11 +10,33 @@ using System.Windows.Forms;
 
 namespace PingPong
 {
-    public partial class Form1 : Form
+    public partial class frmPingPong : Form
     {
-        public Form1()
+        int xDirection = 5;
+        int yDirection = 2;
+        public frmPingPong()
         {
             InitializeComponent();
+            picBall.Location = new Point(75, 25);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            tmrSpiel.Start();
+        }
+
+        private void tmrSpiel_Tick(object sender, EventArgs e)
+        {
+            if(picBall.Location.X >= 600 || picBall.Location.X <= 0)
+            {
+                xDirection = xDirection * -1;
+            }
+            else if (picBall.Location.Y >= 300|| picBall.Location.Y <= 0)
+            {
+                yDirection = yDirection * -1;
+            }
+            picBall.Location = new Point(picBall.Location.X + xDirection, picBall.Location.Y + yDirection);
+       
         }
     }
 }
