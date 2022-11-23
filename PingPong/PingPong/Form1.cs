38a7c492daea0f0a1bc7b8ed77d7829f49c89c15
Janick <janick.spielmann@messageme.ch>
2022-11-22 18:51:10 -0800

    finished Version2

--- PingPong/PingPong/Form1.cs
+++ PingPong/PingPong/Form1.cs
@@ -14,48 +14,71 @@ namespace PingPong
     {
         int xRichtung = 5;
         int yRichtung = 2;
+        int score = 0;
         public frmPingPong()
         {
             InitializeComponent();
-            ovsBall.Location = new Point(75, 25);
-            picSchlägerRechts.Location = new Point(590, 25);
+            
         }
 
         private void btnStart_Click(object sender, EventArgs e)
         {
+            ovsBall.Location = new Point(75, 25);
+            picSchlägerRechts.Location = new Point(580, 50);
+            score = 0;
             tmrSpiel.Start();
         }
 
         private void tmrSpiel_Tick(object sender, EventArgs e)
         {
-            if(ovsBall.Location.X >= 600- ovsBall.Width || ovsBall.Location.X <= 0)
+            
+            
+            if ((ovsBall.Location.Y + (ovsBall.Height/2) > picSchlägerRechts.Location.Y ) && (ovsBall.Location.Y + (ovsBall.Height / 2) < (picSchlägerRechts.Location.Y + picSchlägerRechts.Height) + (ovsBall.Height / 2)))
+            {
+                if (ovsBall.Location.X >= picSchlägerRechts.Location.X - ovsBall.Width)
+                {
+                    xRichtung = xRichtung * -1;
+                    score += 10;
+                    ovsStatus.BackColor = Color.GreenYellow;
+                }
+            }
+            
+
+            if (ovsBall.Location.X >= pnlSpiel.Width - ovsBall.Width)
+            {
+                
+                tmrSpiel.Stop();
+
+                ovsStatus.BackColor = Color.Red;
+            }
+            else if (ovsBall.Location.X <= 0)
             {
                 xRichtung = xRichtung * -1;
             }
-            else if (ovsBall.Location.Y >= 300- ovsBall.Height || ovsBall.Location.Y <= 0)
+
+            else if (ovsBall.Location.Y >= pnlSpiel.Height - ovsBall.Height || ovsBall.Location.Y <= 0)
             {
                 yRichtung = yRichtung * -1;
             }
             ovsBall.Location = new Point(ovsBall.Location.X + xRichtung, ovsBall.Location.Y + yRichtung);
-            picSchlägerRechts.Location = new Point(picSchlägerRechts.Location.X, vsbSchlägerRechts.Location.Y);
+            picSchlägerRechts.Location = new Point(picSchlägerRechts.Location.X, (pnlSpiel.Height / vsbSchlägerRechts.Maximum * vsbSchlägerRechts.Value));
+            txtPunkte.Text = Convert.ToString(score);
 
         }
 
-        private void txtPunkte_TextChanged(object sender, EventArgs e)
-        {
-            int score = 0;
-            
-        }
-
-        private void picSchlägerRechts_Click(object sender, EventArgs e)
+        private void ovsStatus_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void vsbSchlägerRechts_Scroll(object sender, ScrollEventArgs e)
+        private void frmPingPong_Load(object sender, EventArgs e)
         {
-            
+
         }
-      
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
